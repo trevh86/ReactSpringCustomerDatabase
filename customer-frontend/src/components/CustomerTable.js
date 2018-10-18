6f604fac67193bf2016d904fc0f514ce3193a188
trevh86 <trevor.hurt86@gmail.com>
2018-10-18 13:03:16 +0300

    Finally resolving CORS so it not works with front end API calls-tested

--- customer-frontend/src/components/CustomerTable.js
+++ customer-frontend/src/components/CustomerTable.js
@@ -10,19 +10,21 @@ export default class CustomerTable extends Component {
   }
 
   componentDidMount() {
-    fetch("http://localhost:8080/api/customers", { mode: "cors" })
-      .then(res => res.json())
-      .then(resData => {
-        this.setState({ customerData: resData.content });
-      })
-      .catch(err => console.error(err));
+    // fetch("http://localhost:8080/api/customers", { mode: "cors" })
+    //   .then(res => res.json())
+    //   .then(resData => {
+    //     this.setState({ customerData: resData });
+    //   })
+    //   .then(() => console.log(this.state.customerData))
+    //   .catch(err => console.error(err));
+    this.getCustomersFromBackend()
   }
 
   async getCustomersFromBackend() {
     try {
       const response = await axios.get("http://localhost:8080/api/customers");
-      console.log(response);
-      return response;
+      console.log(response.data._embedded.customers);
+      this.setState({ customerData: response.data._embedded.customers});
     } catch (e) {
       console.log(e);
     }
