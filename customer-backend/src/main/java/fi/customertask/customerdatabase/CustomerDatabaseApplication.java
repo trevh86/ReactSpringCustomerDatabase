8875ba13e00abc86e9b61aeda1d5df81b4611fda
trevh86 <trevor.hurt86@gmail.com>
2018-10-23 13:38:41 +0300

    Current configuration working with jetty.

--- customer-backend/src/main/java/fi/customertask/customerdatabase/CustomerDatabaseApplication.java
+++ customer-backend/src/main/java/fi/customertask/customerdatabase/CustomerDatabaseApplication.java
@@ -2,7 +2,6 @@ package fi.customertask.customerdatabase;
 
 import fi.customertask.customerdatabase.domain.Customer;
 import fi.customertask.customerdatabase.domain.CustomerRepository;
-import javafx.application.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +11,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class CustomerDatabaseApplication  extends SpringBootServletInitializer {
+public class CustomerDatabaseApplication extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure
@@ -23,7 +22,7 @@ public class CustomerDatabaseApplication  extends SpringBootServletInitializer {
     @Autowired
     private CustomerRepository customerRepository;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(CustomerDatabaseApplication.class, args);
     }
 
